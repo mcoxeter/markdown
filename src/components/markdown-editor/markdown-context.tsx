85744a8f185be7d22dac070b0e21ec331e1069dc
Michael Coxeter <mcoxeter@gamil.com>
2024-12-20 10:22:12 +0100

    Added context to edit

--- src/components/markdown-editor/markdown-context.tsx
+++ src/components/markdown-editor/markdown-context.tsx
@@ -5,11 +5,15 @@ export type PanelType = 'markdown' | 'ast' | 'html' | 'wysiwyg';
 export type MarkdownContextType = {
   currentPanel: PanelType;
   setCurrentPanel: React.Dispatch<React.SetStateAction<PanelType>>;
+  markdown: string;
+  setMarkdown: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const defaultMarkdownContext: MarkdownContextType = {
   currentPanel: 'wysiwyg',
-  setCurrentPanel: () => {}
+  setCurrentPanel: () => {},
+  markdown: '',
+  setMarkdown: () => {}
 };
 
 export const MarkdownContext = createContext<MarkdownContextType>({
@@ -20,8 +24,11 @@ export const MarkdownContextProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   const [currentPanel, setCurrentPanel] = useState<PanelType>('wysiwyg');
+  const [markdown, setMarkdown] = useState<string>('');
   return (
-    <MarkdownContext.Provider value={{ currentPanel, setCurrentPanel }}>
+    <MarkdownContext.Provider
+      value={{ currentPanel, setCurrentPanel, markdown, setMarkdown }}
+    >
       {children}
     </MarkdownContext.Provider>
   );
--- src/components/markdown-editor/panels/ast-input.tsx
+++ src/components/markdown-editor/panels/ast-input.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './ast-input.module.scss';
 import { MarkdownContext } from '../markdown-context';
+import { MDRootToken } from '../../../compiler/markdown/root/md-root-token';
 
 const ASTInput: React.FC = () => {
   const context = React.useContext(MarkdownContext);
-  if (context.currentPanel !== 'ast') return null;
-  return <textarea className={styles.astinput} />;
+  const [ast, setAST] = React.useState<string>('');
+
+  React.useEffect(() => {
+    const rootToken = new MDRootToken();
+    rootToken.compile(context.markdown, 0, context.markdown.length);
+    setAST(rootToken.getAST());
+  }, [context.markdown]);
+
+  return (
+    <textarea
+      className={styles.astinput}
+      value={ast}
+      onChange={(e) => setAST(e.target.value)}
+    />
+  );
 };
 
 export default ASTInput;
